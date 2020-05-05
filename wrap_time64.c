d28f8c208721630d547523b2601f96c5f615dcc5
Chris Lalancette <clalancette@openrobotics.org>
2020-05-05 10:55:07 -0400

    Add clock_gettime wrapper.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- wrap_time64.c
+++ wrap_time64.c
@@ -7,3 +7,8 @@ int futimens(int fd, const struct timespec tsp[2])
 {
     return syscall(__NR_utimensat, fd, NULL, &tsp[0], 0);
 }
+
+int clock_gettime(clockid_t clock_id, struct timespec *sp)
+{
+    return syscall(__NR_clock_gettime, clock_id, sp);
+}
